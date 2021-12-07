93499167d676d5d67a1893b4fa8226801c8283b4
mimir <yilverjavierm06gmail.com>
2021-12-07 08:21:58 -0600

    Primer commit

---
+++ models/server.js
@@ -0,0 +1,60 @@
+// Servidor de Express
+const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
+const path = require('path');
+
+const Sockets = require('./sockets');
+
+
+class Server {
+
+    constructor() {
+
+        this.app = express();
+        this.port = process.env.PORT;
+
+        // Htttp server
+        this.server = http.createServer( this.app );
+
+        // Configuraciones de sockets
+        this.io = socketio( this.server, { /* Configuraciones  */ } );
+    }
+
+    middlewares() {
+
+        // Desplegar el directorio Publico
+        this.app.use( express.static( path.resolve( __dirname , '../public' )) );
+
+    }
+
+    configurarSockets() {
+        
+        new Sockets( this.io );
+
+    }
+
+    execute() {
+
+        // Inicializar Middlewares
+        this.middlewares();
+
+        // Inicializar Sockets
+        this.configurarSockets();
+
+        // Inicializar Server
+        this.server.listen(this.port, () => {
+            console.log('Server correindo en puerto:', this.port );
+        });
+        
+    }
+
+
+
+
+}
+
+module.exports = Server;
+
+
+
